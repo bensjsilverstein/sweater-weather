469acbb5a9170059384552407eb6795ea4b12b66
Ben Silverstein <benjsilverstein@comcast.net>
2022-09-26 11:54:16 -0600

    Final commit

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,4 +4,5 @@ class Api::V1::ForecastController < ApplicationController
     quantity = params[:quantity]
     forecast = BookForecastFacade.get_book_destination_forecast(location)
     render json: BookSerializer.book_search_response(books, forecast, city)
+  end
 end
