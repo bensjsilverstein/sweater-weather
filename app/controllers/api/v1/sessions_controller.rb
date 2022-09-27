ce02066d0716c02a79a6c310b678845cadc22a5f
Ben Silverstein <benjsilverstein@comcast.net>
2022-09-27 15:27:46 -0600

    Login story complete

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user.nil?
+      render(json: { message: "User does not exist. Please create an account." }, status: 401)
+    elsif user.authenticate(params[:password])
+      render json: UserSerializer.new(user), status: 201
+    else
+      render(json: { message: "User email or password is invalid." }, status: 401)
+    end
+  end
+end
