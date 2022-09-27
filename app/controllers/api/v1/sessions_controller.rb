5cedd356e04a82a63092fa446737ac87f2aa5d20
Ben Silverstein <benjsilverstein@comcast.net>
2022-09-27 16:24:52 -0600

    Modify session controller if statement

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,12 +2,11 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
 
-    if user.nil?
-      render(json: { message: "User does not exist. Please create an account." }, status: 401)
-    elsif user.authenticate(params[:password])
+    if !user.nil? && user.authenticate(params[:password])
       render json: UserSerializer.new(user), status: 201
     else
       render(json: { message: "User email or password is invalid." }, status: 401)
     end
   end
+  
 end
